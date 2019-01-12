58e4a6cd36568e11929724a5efb09e7f6abbb65e
magicmanam <magicmanam@users.noreply.github.com>
2019-01-12 21:44:07 +0300

    Initial commit. Good luck!

---
+++ pro.html.js
@@ -0,0 +1,45 @@
+﻿(function (pro) {
+    'use strict';
+
+    var PRO_HTML_ATTR = 'pro-html',
+        unit = new pro.Unit();
+
+    (function proHTML(children) {
+        var i = 0;
+
+        while (i < children.length) {
+            let child = children[i++];
+
+            if (child.is(PRO_HTML_ATTR)) {
+                let htmlUrl = child.getAttribute(PRO_HTML_ATTR);
+
+                pro.http
+                    .in(htmlUrl)
+                    .on(200, function (response) {
+                        let index = response.indexOf('<center><a href="http://somee.com">Web hosting by Somee.com</a></center>');
+
+                        if (index > -1) {
+                            child.innerHTML = response.substr(0, index);
+                        } else {
+                            child.innerHTML = response;
+                        }
+                        unit.out(htmlUrl, child);
+
+                        proHTML(child.children);
+                    })
+                    .on(404, function () {
+                        console.log(htmlUrl + ': content was not loaded. Try to refresh the page.');
+                    })
+                    .on('end', function () {
+                        child.out(PRO_HTML_ATTR);
+                    })
+                    .out('GET');
+            } else {
+                proHTML(child.children);
+            }
+        }
+    }
+    )(document.children);
+
+    pro.html = unit;
+})(pro);
